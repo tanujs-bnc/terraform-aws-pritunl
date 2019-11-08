8680a7847890b3133e421b5a4c7ab34879236433
Lewis England <lewis2004@hotmail.com>
2019-11-08 23:07:16 +0000

    Add: More outputs to the module (#3)
    * security groups id (security_group_ids, office_security_group_id)
    * aws instance id (aws_instance_id)
    * ami id (aws_ami_id)

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,22 @@ output "pritunl_public_ip" {
   value = "${aws_instance.pritunl.public_ip}"
 }
 
+output "security_group_ids" {
+  value = ["${aws_security_group.pritunl.id}",  "${aws_security_group.allow_from_office.id}"]
+}
+
 output "main_security_group_id" {
   value = "${aws_security_group.pritunl.id}"
+}
+
+output "office_security_group_id" {
+  value = "${aws_security_group.allow_from_office.id}"
+}
+
+output "aws_instance_id" {
+  value = "${aws_instance.pritunl.id}"
+}
+
+output "aws_ami_id" {
+  value = "${data.aws_ami.oracle.id}"
 }
